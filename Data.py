a6e706ed559a4048e80fa05bda531e42c2978e10
Kyle Melton <fs2dwyab@gmail.com>
2018-10-13 12:55:44 -0600

    creating data management object

---
+++ Data.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class BST_FIELD(Enum):
+    """"
+    values castable to doubles will be assigned values less than 100
+    values castable only as strings will be assigned values greater than 200
+    """"
+
+class DataPacket:
+    def __init__():
+        self.data = {}
+    
+    def get_field(self, field):
+        return data[field]
+    
+    def set_field(self, field, value):
+        if typeof(field) is BST_FIELD:
+            data[field] = value
+            return 1
+        return 0
+        
+    
